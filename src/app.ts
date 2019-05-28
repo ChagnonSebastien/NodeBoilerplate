698d3ee4992731a25d045d8b63f57fba8af14324
Sebastien Chagnon <chagnon.s21@gmail.com>
2019-05-27 20:11:14 -0400

    added a sample route

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express, { Request, Response, NextFunction } from 'express';
+import { SampleRoute } from './routes/SampleRoute';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.send('Hello World!');
-});
+let sampleRoute: SampleRoute = new SampleRoute();
+
+let router: express.Router;
+router = express.Router();
+
+router.get('/', sampleRoute.get)
+
+app.use('/api', router);
 
 app.listen(port, () => console.log(`Server is listening on ${port}`));
